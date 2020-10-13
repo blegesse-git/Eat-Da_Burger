7213d34734490bd134e09f9961a0f435e72303fe
Bethelhem Legesse <legessebl@gmail.com>
2020-10-13 12:48:49 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    port: 3306,
-    database: "burgers_db",
-    password: "Kkjowqstr1p"
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "burgers_db",
+        password: "Kkjowqstr1p"
+    });
+
+}
+
 
 connection.connect(function(err){
     if(err) {
